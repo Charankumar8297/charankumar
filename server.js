dc39e8247156dee9937af863ce41c2b63cdec76f
Charankumar8297 <charankumar8297@gmail.com>
2025-04-02 20:37:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const employeeRoutes = require('./routes/employeeRoutes');
+const cors = require("cors")
+
+const app = express();
+
+dotEnv.config();
+app.use(cors())
+
+app.use(bodyParser.json())
+
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => {
+    console.log(" MongoDB Connected Successfully");
+}).catch((error) => {
+    console.error(" MongoDB Connection Error:", error);
+});
+
+app.use(bodyParser.json()); 
+app.use('/employees', employeeRoutes);
+
+const PORT = process.env.PORT || 5000;
+console.log(process.env);
+
+app.listen(PORT, () => {
+    console.log(` Server Started and running at ${PORT}`);
+});
